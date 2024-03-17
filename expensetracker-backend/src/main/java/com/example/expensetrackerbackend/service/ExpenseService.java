0391ae3dfaeeb49ffabf2c183d5cb2273b80a432
Yutika Singh <96788861+yutika-singh@users.noreply.github.com>
2024-03-17 16:23:25 +0530

    Update ExpenseService.java

--- expensetracker-backend/src/main/java/com/example/expensetrackerbackend/service/ExpenseService.java
+++ expensetracker-backend/src/main/java/com/example/expensetrackerbackend/service/ExpenseService.java
@@ -11,7 +11,7 @@ import com.example.expensetrackerbackend.entity.Expense;
 @Service
 public class ExpenseService {
 
-    public List<Expense> handleExpensesAndGenerateEstimates(List<Expense> expenses){
+    public List<Expense> handleExpensesAndGenerateEstimates(List<Expense> expenses, String level){
         // Define category priorities
         Map<String, Integer> categoryPriorities = new HashMap<>();
         categoryPriorities.put("education", 1);
@@ -23,11 +23,28 @@ public class ExpenseService {
         categoryPriorities.put("takeaways", 3);
         categoryPriorities.put("other", 3);
 
+        int lvl;
+        
+        switch (level) {
+            case "little":
+                lvl = 1;
+                break;
+            case "moderate":
+                lvl = 2;
+                break;
+            case "lot":
+                lvl = 3;
+                break;
+            default:
+                lvl = 2; // Default to moderate saving
+                break;
+        }
+
         // Calculate modified expenses
         for (Expense expense : expenses) {
             Integer priority = categoryPriorities.get(expense.getCategory());
             if (priority != null) {
-                double adjustmentPercentage = getAdjustmentPercentage(priority.intValue());
+                double adjustmentPercentage = getAdjustmentPercentage(priority.intValue(),lvl);
                 double modifiedAmount = expense.getAmount() * (1 - adjustmentPercentage);
                 expense.setAmount(modifiedAmount);
             }
@@ -36,14 +53,14 @@ public class ExpenseService {
         return expenses;
     }
 
-    private double getAdjustmentPercentage(int categoryPriority) {
+    private double getAdjustmentPercentage(int categoryPriority, int lvl) {
         switch (categoryPriority) {
             case 1:
                 return 0.0; // No adjustment for most essential categories
             case 2:
-                return 0.05; // 5% adjustment for less essential categories
+                return lvl*0.05; // lvl*5% adjustment for less essential categories
             case 3:
-                return 0.10; // 10% adjustment for least essential categories
+                return lvl*0.10; // lvl*10% adjustment for least essential categories
             default:
                 return 0.00;
         }
