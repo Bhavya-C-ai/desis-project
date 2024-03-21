8a3bb45b2898d0320d617624d08ada169741187b
Yutika Singh <96788861+yutika-singh@users.noreply.github.com>
2024-03-22 00:01:21 +0530

    Update User.java

--- expensetracker-backend/src/main/java/com/example/expensetrackerbackend/entity/User.java
+++ expensetracker-backend/src/main/java/com/example/expensetrackerbackend/entity/User.java
@@ -6,10 +6,8 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 import java.util.Date;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import javax.persistence.Entity;
+import javax.persistence.Id;
 
 @Getter
 @Setter
