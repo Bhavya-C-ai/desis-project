4432376d03695a07ed64023aae223f59accbd27b
Yutika Singh <96788861+yutika-singh@users.noreply.github.com>
2024-03-21 23:15:07 +0530

    Update for streak

--- expensetracker-backend/src/main/java/com/example/expensetrackerbackend/repository/ExpenseRepository.java
+++ expensetracker-backend/src/main/java/com/example/expensetrackerbackend/repository/ExpenseRepository.java
@@ -1,11 +1,16 @@
 package com.example.expensetrackerbackend.repository;
 
+import java.util.Date;
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import com.example.expensetrackerbackend.entity.Expense;
 
 public interface ExpenseRepository extends JpaRepository<Expense,Long>{
     List<Expense> findAll();
+    @Query("SELECT e.createdAt FROM Expense e ORDER BY e.createdAt DESC")
+    Optional<Date> findLastAddedDate();
 }
